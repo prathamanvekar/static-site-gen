8e015d70aa5a138ed0e5234bed0fc49a95c6e274
Prathamesh Anvekar <anvekarprathamesh13@gmail.com>
2025-12-17 01:40:01 +0530

    deployed YAY

--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from generate_page import generate_pages_recursive
 
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
+    basepath = "/"
+    if sys.argv[1]:
+        basepath = sys.argv[1]
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -20,8 +25,7 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating page...")
-    generate_pages_recursive(
-        dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 
